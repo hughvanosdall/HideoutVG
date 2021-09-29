6da07485498976bc4dad7800985bd7a44cea153a
John Cripe <jcripe@webtraxllc.com>
2021-09-29 02:20:59 -0700

    added whole card link

--- components/ArticleCard.js
+++ components/ArticleCard.js
@@ -5,56 +5,77 @@ import Image from 'next/image'
 export default function Article({article}) {
     const {title, slug, thumbnail} = article.fields;
     return (
-        <div className="article-card">
-            <div className="featured">
-                <Image 
-                    className="thumbnail"
-                    src={`https:${thumbnail.fields.file.url}`}
-                    width={400}
-                    height={200}
-                    
-                />
-            </div>
-            <div className="content">
-                <div className="info">
-                    {title}
-                </div>
-                <div className="actions">
-                    <Link href={`/articles/${slug}`}><a>Read More</a></Link>
-                </div>
-                
-            </div>
-            
-            <style jsx>
-                {`
-                    .article-card {
-                        display: flex;
-                        border-bottom: 4px dotted blue;
-                        padding: 20px 40px
-                    }
-
-                    .featured {
-                        max-width: 400px;
-                    }
-
-                    .content {
-                        display: flex;
-                        flex-direction: column;
-                        align-content: flex-start;
-                        justify-content: space-around;
-                        align-items: flex-start;
-
-                        padding: 50px;
-                    }
-
-                    .actions a {
-                        color: #fff;
-                        background: #f01b29;
+        <Link href={`/articles/${slug}`}>
+            <div className="article-card">
+                <div className="featured">
+                    <Image 
+                        className="thumbnail"
+                        src={`https:${thumbnail.fields.file.url}`}
+                        width={400}
+                        height={200}
                         
-                        text-decoration: none;
-                    }
-                `}
-            </style>
-        </div>
+                    />
+                </div>
+                <div className="content">
+                    <div className="info">
+                        {title}
+                    </div>
+                    <div className="actions">
+                        {/* <Link href={`/articles/${slug}`}><a>Read More</a></Link> */}
+                    </div>
+                    
+                </div>
+                {/* <div className="featured">
+                    <Image 
+                        className="thumbnail"
+                        src={`https:${thumbnail.fields.file.url}`}
+                        width={400}
+                        height={200}
+                        
+                    />
+                </div>
+                <div className="content">
+                    <div className="info">
+                        {title}
+                    </div>
+                    <div className="actions">
+                        <Link href={`/articles/${slug}`}><a>Read More</a></Link>
+                    </div>
+                    
+                </div> */}
+                
+                <style jsx>
+                    {`
+                        .article-card {
+                            display: flex;
+                            border-bottom: 2px solid lightgray;
+                            padding: 20px 40px
+                        }
+                        .article-card:hover {
+                            cursor: pointer;
+                        }
+    
+                        .featured {
+                            max-width: 400px;
+                        }
+    
+                        .content {
+                            display: flex;
+                            flex-direction: column;
+                            align-content: flex-start;
+                            justify-content: space-around;
+                            align-items: flex-start;
+    
+                            padding: 50px;
+                        }
+    
+                        .actions a {
+                            color: #6C6C6C;
+                            text-decoration: none;
+                        }
+                    `}
+                </style>
+            </div>
+        </Link>
     )
 }
