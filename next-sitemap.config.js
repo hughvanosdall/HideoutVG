d8a620e09882dc9ff90e16306fe6fafbea6ede43
Hugh <40128901+hughvanosdall@users.noreply.github.com>
2022-04-21 00:20:04 -0700

    Fixed Sitemapping

---
+++ next-sitemap.config.js
@@ -0,0 +1,24 @@
+// let policy = {
+//     userAgent: "*",
+// };
+
+// if (process.env.ENVIRONMENT !== "production") {
+//     policy.disallow="/";
+// };
+
+// module.exports = {
+//     siteUrl: process.env.URL,
+//     generateRobotsTxt: true,
+//     robotsTxtOptions: {
+//         policies: [
+//             policy
+//         ],
+//     },
+//     outDir: "./out"
+// }
+
+let siteUrl = 'https://hideoutvg.com/'
+
+module.exports = {
+    siteUrl,
+}
